bc462141eca653a1588b59caf0fc200457bc6315
bthank <bthankachan@yahoo.com>
2021-07-16 08:50:08 -0400

    initial commit

---
+++ src/main/java/com/binu/hibernate/demo/CreateStudentImagesMapDemo.java
@@ -0,0 +1,63 @@
+package com.binu.hibernate.demo;
+
+import java.util.Map;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.binu.hibernate.demo.entity.Student;
+
+public class CreateStudentImagesMapDemo {
+
+	
+	public static void main(String[] args) {
+		
+		
+		// create session factory
+		SessionFactory factory = new Configuration()
+									.configure("hibernate.cfg.xml")
+									.addAnnotatedClass(Student.class)
+									.buildSessionFactory();
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			
+		
+		// create the object
+			Student tempStudent = new Student("Jill","Fournier","jill_fournier@msn.com");
+			Map<String,String> theImages = tempStudent.getImages();
+			
+			theImages.put("photo2.jpg","Photo 2");
+			theImages.put("photo3.jpg","Photo 3");
+			theImages.put("photo4.jpg","Photo 4");
+			theImages.put("photo5.jpg","Photo 5");   
+		
+		// start a transaction
+			session.beginTransaction();
+		
+		// save the object
+			System.out.println("Saving the student and images ...");
+			session.persist(tempStudent);
+		
+		// commit the transaction
+			session.getTransaction().commit();
+			System.out.println("Done saving!!");
+			
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		// do code cleanup
+		finally {
+			session.close();
+			factory.close();
+		}
+		
+		
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/binu/hibernate/demo/entity/Student.java
@@ -0,0 +1,102 @@
+package com.binu.hibernate.demo.entity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;  // javax.persistence are the JPA annotations
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MapKeyColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="student")
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	@Column(name="first_name")
+	private String firstName;
+	@Column(name="last_name")
+	private String lastName;
+	@Column(name="email")
+	private String email;
+	
+	
+	// Map the element collection
+	
+	// create collection to hold images for the student
+	@ElementCollection
+	@CollectionTable(name="image")
+	@MapKeyColumn(name="file_name")  // this is the key column for the collection
+	@Column(name="image_name")       // this is the value column for the collection
+	private Map<String, String> images = new HashMap<String, String>();
+	
+	
+
+	public Student() {
+		super();
+		 
+	}
+
+	public Student(String firstName, String lastName, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Map<String, String> getImages() {
+		return images;
+	}
+
+	public void setImages(Map<String, String> images) {
+		this.images = images;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", images=" + images + "]";
+	}
+
+	
+}
